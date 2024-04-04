a41073329194ccfd75d90fc6883e5a387acb9ce3
Isa Yasin <iyet.alif.lam@gmail.com>
2024-04-04 09:48:51 +0000

    express

--- myApp.js
+++ myApp.js
@@ -2,9 +2,10 @@ let express = require('express');
 let app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello Express");
+    res.sendFile( __dirname + "/views/index.html");
 })
 
+app.use(express.static(__dirname + "/public"))
 
 
 
