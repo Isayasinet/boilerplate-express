a683aaf6b5528e8aea18e72d83495f04ae411d1a
Isa Yasin <iyet.alif.lam@gmail.com>
2024-04-04 09:50:01 +0000

    express

--- myApp.js
+++ myApp.js
@@ -5,7 +5,8 @@ app.get("/", (req, res) => {
     res.sendFile( __dirname + "/views/index.html");
 })
 
-app.use(express.static(__dirname + "/public"))
+app.use(__dirname + "/public", express.static(__dirname + "/public"))
+
 
 
 
